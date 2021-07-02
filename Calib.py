71f249c20a7bed40056a173d16540df27d34ced9
Filippo Falezza <filippo.falezza@outlook.it>
2021-07-02 10:59:26 +0100

    TEST

--- Calib.py
+++ Calib.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 import numpy as np
-
 #from libtiff import TIFF
 import matplotlib.pyplot as plt
+from matplotlib.widgets import RectangleSelector
 from scipy import ndimage
 from PIL import Image
 import tifffile as tf
@@ -74,16 +74,35 @@ class Calibrate():
         #TODO: needs path selection capability and dest folder
         img = f'{self.PATH_SOURCE}/{self.file_list[i]}'
         image = cv2.imread(img, -1)#keep img as-is: 16bit tiff
+        fig, current_ax = plt.subplots()
+        plt.imshow(image)
+        print("\n      click  -->  release")
+
+        # drawtype is 'box' or 'line' or 'none'
+        toggle_selector.RS = RectangleSelector(current_ax, self.position,
+            drawtype='box', useblit=True, button=[1, 3],  # don't use middle button
+            minspanx=5, minspany=5, spancoords='pixels', interactive=True)
+        plt.connect('key_press_event', toggle_selector)
+        plt.show()
+        pdb.set_trace()
         #ROI manual selection
-        x,y,w,h = cv2.selectROI(image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        #x,y,w,h = cv2.selectROI(image)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+        x,y,w,h = self.ROI_data
         ROI = image[y:y+h, x:x+w]
-        cv2.imshow(f'ROI {i}',ROI)
+        cv2.imshow(f'ROI {i}',ROI)#Should use matplotlib for this?
         cv2.imwrite(f'{self.PATH_TARGET}/ROI_{i}.tif',ROI, ((int(cv2.IMWRITE_TIFF_COMPRESSION), 1)))
         self.ROI_list.append(f'{self.PATH_TARGET}/ROI_{i}.tif')
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+    def position(self, eclick, erelease):
+        self.ROI_data = []
+        x1, y1 = eclick.xdata, eclick.ydata
+        x2, y2 = erelease.xdata, erelease.ydata
+        self.ROI_data = [x1,y1,x2,y2]
+        #return self.ROI_data
 
     def ROI_all(self):
         """
@@ -162,8 +181,61 @@ class Calibrate():
         with open(namefile + '.pkl', 'rb') as f:
             self.Data = eval(repr(pickle.load(f)))
 
+#MatPlotLib stuff, as in https://matplotlib.org/2.0.2/examples/widgets/rectangle_selector.html
+#The following is debug only
+def line_select_callback(eclick, erelease):
+    'eclick and erelease are the press and release events'
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+    print("(%3.2f, %3.2f) --> (%3.2f, %3.2f)" % (x1, y1, x2, y2))
+    print(" The button you used were: %s %s" % (eclick.button, erelease.button))
+    public_ROI = [x1,y1,x2,y2] #need to make this private,a s this is rubbish coding
+
+def toggle_selector(event):
+    print(' Key pressed.')
+    """
+    if event.key in ['Q', 'q'] and toggle_selector.RS.active:
+        print(' RectangleSelector deactivated.')
+        toggle_selector.RS.set_active(False)
+    if event.key in ['A', 'a'] and not toggle_selector.RS.active:
+        print(' RectangleSelector activated.')
+        toggle_selector.RS.set_active(True)
+    """
+    if event.key in ['Enter'] and toggle_selector.RS.active:
+        toggle_selector.RS.set_active(False)
+
 #TODO: fitting of Calibration
+class Fitting():
+    def __init__(self, data):
+        OD = []
+        Dose = []
+        #DEBUG
+        #pdb.set_trace()
+        for i in data:
+            if isinstance(i, int):
+                OD.append(data[i][0])
+                Dose.append(data[i][1])
+        #TODO: rearrange array as it is by increasing dose
+        self.Array = [OD, Dose]
+
+    def plot(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        #ax.plot(self.Array[0], np.flip(self.Array[1]), 'bo', label='test')
+        ax.plot(self.Array[0], self.Array[1], 'bo', label='test')
+        ax.set_title('OD vs Dose')
+        ax.set_xlabel('Optical Density')
+        ax.set_ylabel('Dose [Gy]')
+        ax.legend(loc='upper left')
+        plt.show()
+
+    def fit(self):
+        return None
 
 if __name__ == '__main__':
-    a = Crop()
-    a.load('dictionary')
+    #from Calib import Calibrate, Fitting
+    a = Calibrate('./')
+    a.ROI_single(1)
+    #a.load('dictionary')
+    #b = Fitting(a.Data)
+    #b.plot()
